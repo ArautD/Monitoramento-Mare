0e45f59702badda038fa7987315a83279362df84
ArautD <contatopaulolemosgit@gmail.com>
2026-02-03 20:05:01 -0300

    feat: Adicao de segurança de acesso a API

--- services/clima.py
+++ services/clima.py
@@ -1,9 +1,7 @@
 import requests
 
-API_KEY = "ed94a97b9438efad1ef150fdc4fe49cd"
-
 def obter_dados_clima(cidade): 
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={API_KEY}&units=metric"
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={API_KOPENWEATHER_API_KEY}&units=metric"
 
     response = requests.get(url)
     data = response.json()
@@ -18,7 +16,7 @@ def obter_dados_clima(cidade):
     }
 
 def obter_prev_chuva(cidade):
-    url = f"http://api.openweathermap.org/data/2.5/forecast?q={cidade}&appid={API_KEY}&units=metric"
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={cidade}&appid={OPENWEATHER_API_KEY}&units=metric"
 
     response = requests.get(url)
 
