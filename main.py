e96043729ebc01f851fcd4defee1464252b02b76
ArautD <contatopaulolemosgit@gmail.com>
2026-02-03 12:43:11 -0300

    feat: Adicao de Funcoes para clima atual e para previsao

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from services.clima import obter_dados_climaticos
+from services.clima import obter_prev_chuva
+from services.clima import obter_dados_clima
 from rules.risco import calcular_risco
 
 app = FastAPI()
@@ -9,13 +10,32 @@ app = FastAPI()
 def home():
     return {"message": "API de Monitoramento de Maré e Chuva"}
 
-@app.get("/status")
-def status(cidade: str):
+@app.get("/status-atual")
+def status_atual(cidade: str):
     dados = obter_dados_clima(cidade)
     risco = calcular_risco(dados)
+
     return {
         "cidade": cidade,
-        "dados_climaticos": dados,
+        "chuva_mm": dados["chuva"],
+        "mare_m": dados["mare"],
         "risco": risco
     }
 
+@app.get("/status-previsao")
+def status_previsao(cidade: str):
+    chuva_prevista = obter_prev_chuva(cidade)
+    risco = calcular_risco(dados)
+
+    dados = {
+        "chuva": chuva_prevista,
+        "mare": 2.5
+    }
+
+
+    return {
+        "cidade": cidade,
+        "chuva_prevista_24_mm": chuva_prevista,
+        "mare_m": dados["mare"],
+        "risco": risco
+    }
--- services/clima.py
+++ services/clima.py
@@ -1,9 +1,37 @@
 import requests
 
-API_KEY = "sua_chave_api_aqui" # lembrar de por
+API_KEY = "ed94a97b9438efad1ef150fdc4fe49cd"
 
 def obter_dados_clima(cidade): 
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={API_KEY}&units=metric"
+
+    response = requests.get(url)
+    data = response.json()
+
+    chuva = 0
+    if "rain" in data:
+        chuva = data["rain"].get("1h", 0)
+    mare = 2.5  # valor fixo para simulação
     return {
-        "chuva": 55,
-        "mare": 2.7
+        "chuva": chuva,
+        "mare": mare
     }
+
+def obter_prev_chuva(cidade):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={cidade}&appid={API_KEY}&units=metric"
+
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print("Erro de API: ", response.text)
+        return 0
+    
+    data = response.json()
+
+    total_chuva = 0
+
+    for item in data["list"]:
+        chuva = item.get("rain", {}).get("3h", 0)
+        total_chuva += chuva
+
+    return round(total_chuva,2)
